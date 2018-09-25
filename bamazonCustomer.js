7db2bae2926793895debc97ec321e8abde389d37
Mobious <sammiel.gross@gmail.com>
2018-09-25 11:12:33 -0500

    shell

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,14 @@ var request = require("request");
 var inquirer = require("inquirer");
 var mysql =  require("mysql");
 
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+
+	// Your username
+	user: 'root',
+
+	// Your password
+	password: 'password',
+	database: 'Bamazon'
+});
